311cc7017703bf97c5446e0729c07b89c887a2a8
Kim Pellikaan <kim@web-flight.nl>
2015-04-22 00:30:40 +0200

    Due to rename of init lib file I changed the object and method names.

--- bin/layback.js
+++ bin/layback.js
@@ -2,7 +2,7 @@
 /*eslint no-console:0*/
 
 var program = require("commander");
-var validate = require("../lib/validator.js");
+var layback = require("../lib/layback.js");
 
 program
     .version("0.0.1")
@@ -10,7 +10,7 @@ program
     .alias("val")
     .description("Validate swagger documentation. Pass path to swagger.json and Url to the API.")
     .action(function(path, host){
-        validate.documentation(path, host, function(err, result) {
+        layback.validate(path, host, function(err, result) {
             if(err) { return console.log(err); }
             return console.log(result);
         });
